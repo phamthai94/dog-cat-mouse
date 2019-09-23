1d9d306215a94b464a5bdcb9c8752170102a7e30
Pham Thai <thaiphamfx@gmail.com>
2019-09-23 16:28:31 +0700

    chuyển màu grey sang magenta

--- dog.js
+++ dog.js
@@ -9,6 +9,6 @@ Dog.prototype.eat = function(cat){
     this.stomatch.push(cat)
 }
 Dog.prototype.sayHi = function(){
-    console.log('Hi ! i am a dog, my name is '+ chalk.grey(this.name))
-};
+    console.log('Hi ! i am a dog, my name is '+ chalk.magenta.bold.italic(this.name))
+}
 module.exports = Dog;
