dbcaee3fa1af5b4317cc60d2900b7ac46eadb800
Pham Thai <thaiphamfx@gmail.com>
2019-09-23 20:35:18 +0700

    add var b

--- helloworld.js
+++ helloworld.js
@@ -5,3 +5,4 @@ console.log('hello world');
 console.log(chalk.magenta.bold.italic('my name is thái'));
 console.log(chalk.magenta.bold.italic('Hi '+ readline.question('input your name?')));
 var a = 'nakfhkahf';
+var b = 'b';
