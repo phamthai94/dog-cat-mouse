13ec24215f8fbc0eea36a1ffd61c50c0a867b6db
Pham Thai <thaiphamfx@gmail.com>
2019-09-23 18:40:37 +0700

    add var a

--- helloworld.js
+++ helloworld.js
@@ -4,3 +4,4 @@ var readline = require('readline-sync');
 console.log('hello world');
 console.log(chalk.magenta.bold.italic('my name is thái'));
 console.log(chalk.magenta.bold.italic('Hi '+ readline.question('input your name?')));
+var a = 'nakfhkahf';
